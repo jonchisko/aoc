2cb0d654bb13074590e8aa3b9d9ef04e6de2dc25
DESKTOP-D6AFFRA\Jon <jonchisko@gmail.com>
2024-06-23 00:12:37 +0200

    Add base structs for aoc3

--- src/aoc3.rs
+++ src/aoc3.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 use crate::aoc_traits::{Solution, SolveAdvent};
 
@@ -21,5 +21,35 @@ impl SolveAdvent for Aoc3 {
 
     fn compute_solution2(&self) -> Solution {
         todo!();
+    }
+}
+
+#[derive(Hash, PartialEq, PartialOrd, Ord, Eq, Clone, Copy)]
+struct Point(i32, i32);
+
+struct Number(i32, Vec<Point>); // memory footprint might be made smaller, by using some smart pointer to a slice.
+                                // ... since it will mostly be read and not written to, after the construction
+
+struct Schematic {
+    symbols: HashSet<Point>,
+    numbers: Vec<Number>,
+}
+
+impl Schematic {
+    pub fn new() -> Schematic {
+        Schematic {
+            symbols: HashSet::new(),
+            numbers: Vec::new(),
+        }
+    }
+
+    pub fn add_symbol(&mut self, point: Point) {
+        if self.symbols.insert(point) {
+            panic!("Point was already contained");
+        }
+    }
+
+    pub fn add_number(&mut self, number: Number) {
+        self.numbers.push(number);
     }
 }
